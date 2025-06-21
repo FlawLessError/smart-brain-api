c13f8c0ba12c66f26d4841a6c5778c4ece76adf3
FlawLessError <50498066+FlawLessError@users.noreply.github.com>
2025-06-21 12:57:37 +0100

    Fix issue with postgres tables

--- postgres/deploy-schemas.sql
+++ postgres/deploy-schemas.sql
@@ -2,5 +2,5 @@
 SELECT 'CREATE DATABASE smart-brain' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'smart-brain')\gexec
 
 -- Deploy fresh database tables schemas
-\i /docker-entrypoint-initdb.d/tables/users.sql
-\i /docker-entrypoint-initdb.d/tables/login.sql
+\i /docker-entrypoint-initdb.d/users.sql
+\i /docker-entrypoint-initdb.d/login.sql
